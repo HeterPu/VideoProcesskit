16a15654e4f5729536f17721fe15f853486911b1
peterhu <wycgpeterhu@sina.com>
2018-06-29 11:41:37 +0800

    initialize project

---
+++ VideoProcessKitTest/VideoProcessKitTest/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  VideoProcessKitTest
+//
+//  Created by Peter Hu on 2018/6/29.
+//  Copyright © 2018年 PeterHu. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
